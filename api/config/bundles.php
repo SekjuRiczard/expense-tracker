224756b66e5733b146bcf8f04eace29f9e6e4169
SekjuRiczard <106740608+SekjuRiczard@users.noreply.github.com>
2026-04-06 18:59:51 +0200

    Feat/pin (#3)
    * feat(auth): work in progress - user and security layer
    
    * feat: user authentication
    
    * feat: add pin functionality

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -8,5 +8,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
-    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
