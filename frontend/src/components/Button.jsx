01412fbe9b3bb37c1e8e318a01e4ee65386eb3aa
SekjuRiczard <dawidosak32@gmail.com>
2026-06-08 11:18:52 +0000

    chore(frontend): frontend project setup

--- frontend/src/components/Button.jsx
+++
@@ -1,4 +0,0 @@
-export const Button = ({ children, ...props }) => {
-
-    return <button {...props} style={{ padding: '10px 20px', cursor: 'pointer', ...props.style }}>{children}</button>;
-};
--- frontend/src/components/Input.jsx
+++
@@ -1,3 +0,0 @@
-export const Input = ({ ...props }) => {
-    return <input {...props} style={{ padding: '10px', margin: '10px 0', width: '100%', ...props.style }} />;
-};
