d55fa3b897ebfeb6a6ace739755bfd373e57c51a
SekjuRiczard <dawidosak32@gmail.com>
2026-06-15 14:49:53 +0000

    feat(demo-data): demo data provider module

---
+++ api/src/DemoData/Exception/DemoDataException.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Expense Tracker.
+ *
+ *  (c) SekjuRiczard <dawidosak32@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\DemoData\Exception;
+
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
+
+final class DemoDataException extends ConflictHttpException
+{
+    public static function alreadyGenerated(): self
+    {
+        return new self(
+            'Demo data cannot be generated because the user already has wallets.',
+        );
+    }
+}
